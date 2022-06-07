35e8ab7f6084b18780c638b9b6404d75dfbbabcb
jichengtang <jichengtang@tencent.com>
2022-06-07 16:21:46 +0800

    [Update] 调整编译产物的目录

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+mkdir -p bin
 g++ -std=c++11 ./gen_encoder_layers.cpp -o gen
+mv ./gen ./bin
 g++ -std=c++11 ./format_json.cpp -o format
+mv ./format ./bin
 g++ -std=c++11 ./vocab_converter.cpp -o vocab_converter
+mv ./vocab_converter ./bin
